ea1352b3d4927e23bd14be3f973f3abb4c1dbe61
sharvinbasil <88567635+sharvinbasil@users.noreply.github.com>
2021-10-27 18:57:30 +0530

    Update script.py

--- script.py
+++ script.py
@@ -52,12 +52,4 @@ See <i>/help</i> for commands and more details.</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
-
-⭕️<b>Creater :</b> @TroJanzHEX    
-
-⭕️<b>Language :</b> <code>Python3</code>
-
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-"""
+   
